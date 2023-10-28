5eb5c376c72e94e369c971df7f65fe567b9862bf
Lauriichan <lauriichan@playuniverse.org>
2023-10-28 15:02:45 +0200

    Add SourceTransformerUtils

---
+++ src/main/java/me/lauriichan/maven/sourcemod/api/SourceTransformerUtils.java
@@ -0,0 +1,67 @@
+package me.lauriichan.maven.sourcemod.api;
+
+import java.lang.annotation.Annotation;
+
+import org.jboss.forge.roaster.model.source.AnnotationSource;
+import org.jboss.forge.roaster.model.source.AnnotationTargetSource;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaSource;
+import org.jboss.forge.roaster.model.source.MethodSource;
+
+public final class SourceTransformerUtils {
+
+    private SourceTransformerUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void removeMethod(final JavaClassSource source, final String name) {
+        final MethodSource<JavaClassSource> method = source.getMethod(name);
+        if (method == null) {
+            return;
+        }
+        source.removeMethod(method);
+    }
+
+    public static void removeMethod(final JavaClassSource source, final String name, final Class<?>... types) {
+        final MethodSource<JavaClassSource> method = source.getMethod(name, types);
+        if (method == null) {
+            return;
+        }
+        source.removeMethod(method);
+    }
+
+    public static void removeMethod(final JavaClassSource source, final String name, final String... types) {
+        final MethodSource<JavaClassSource> method = source.getMethod(name, types);
+        if (method == null) {
+            return;
+        }
+        source.removeMethod(method);
+    }
+
+    public static void importClass(final JavaClassSource source, final Class<?> clazz) {
+        if (source.hasImport(clazz)) {
+            return;
+        }
+        source.addImport(clazz);
+    }
+
+    public static void importClass(final JavaClassSource source, final String className) {
+        if (source.hasImport(className)) {
+            return;
+        }
+        source.addImport(className);
+    }
+
+    public static <E extends JavaSource<E>> void removeAnnotation(final AnnotationTargetSource<E, ?> source,
+        final Class<? extends Annotation> annotation) {
+        final AnnotationSource<E> annotationSource = source.getAnnotation(annotation);
+        if (annotationSource != null) {
+            source.removeAnnotation(annotationSource);
+        }
+    }
+
+    public static boolean hasSuperType(final JavaClassSource source, final Class<?> clazz) {
+        return source.getSuperType() != null && source.getSuperType().equals(clazz.getName());
+    }
+
+}
